07949753770e2fe87786fab2899b6da832f7ccc6
life <2421174533@qq.com>
2018-08-12 09:00:10 +0800

    add nand

--- 2440/Nand/Nand.c
+++ 2440/Nand/Nand.c
@@ -25,38 +25,134 @@
 #include <asm/arch/regs-nand.h>
 #include <asm/arch/nand.h>
 
-static struct nand_chip nand;
-static struct mtd_info * mtd;
+#define TACLS  0 
+#define TWRPH0 1
+#define TWRPH1 0
 
-static void s3c2440_select_chip()
+
+struct s3c_nand_regs{
+     unsigned long    nfcont    ; 
+     unsigned long    nfcmd     ; 
+     unsigned long    nfaddr    ; 
+     unsigned long    nfdata    ; 
+     unsigned long    nfeccd0   ; 
+     unsigned long    nfeccd1   ; 
+     unsigned long    nfeccd    ; 
+     unsigned long    nfstat    ; 
+     unsigned long    nfestat0  ; 
+     unsigned long    nfestat1  ; 
+     unsigned long    nfmecc0   ; 
+     unsigned long    nfmecc1   ; 
+     unsigned long    nfsecc    ; 
+     unsigned long    nfsblk    ; 
+     unsigned long    nfeblk    ; 
+
+};
+static struct nand_chip * bsp_nand;
+static struct mtd_info * bsp_mtd;
+static struct s3c_nand_regs * s3c_nand_regs;
+
+
+/*
+static struct mtd_paratition s3c_nand_parts[]={
+    [0]={
+        .name = "bootoader",
+        .size = 0x00040000,
+        .offset = 0,
+    },
+    [1]={
+        .name = "params",
+        .size = MTDPART_OFS_APPEND,
+        .offset= 0X00020000,
+    },
+    [0]={
+        .name = "kernel",
+        .size = MTDPART_OFS_APPEND,
+        .offset=0x00200000,
+    },
+    [0]={
+        .name = "root",
+        .size = MTDPART_OFS_APPEND,
+        .offset= MTDPART_SIZ_FULL,
+    }
+};*/
+
+ 
+static void s3c2440_select_chip(struct mtd_info * mtd,int chipnr)
 {
-
+    if(chipnr == -1)
+    {
+        /*取消选中 NFCOTN[1]设为1*/
+        s3c_nand_regs->nfcont |=  (1<<1);
+        
+    }
+    else
+    {
+        /*选中NFCOTN[1]设为0*/
+        s3c_nand_regs->nfcont &= ~(1<<1);
+    }
 }
 
+static void s3c_cmd_ctrl(struct mtd_info *mtd, int data, unsigned int ctrl)
+{
+
+	if (ctrl & NAND_CLE)
+	{
+        /*发命令NFCMD =DATA*/
+        s3c_nand_regs->nfcmd = data ;
+	}
+	else
+	{
+        /*发命令NFCMD =DATA*/
+        s3c_nand_regs->nfaddr = data ;
+	}
+}
+
+static int s3c_dev_ready(struct mtd_info *mtd)
+{
+    return (s3c_nand_regs->nfstat & (1<<0));
+}
 
 static int  nand_init()
 {
+    struct clk * clk;
     /*1.分配一个nand_chip结构体*/
-    nand = kzalloc(sizeof(struct nand_chip), GFP_KERNEL);
-    
+    bsp_nand = kzalloc(sizeof(struct nand_chip), GFP_KERNEL);
+    s3c_nand_regs = ioremap(0x4E000000,sizeof(struct s3c_nand_regs));
     /*2.设置*/
-    nand ->select_chip = s3c2440_select_chip;
-    
+    bsp_nand->select_chip = s3c2440_select_chip;
+    bsp_nand->cmd_ctrl = s3c_cmd_ctrl;
+    bsp_nand->IO_ADDR_R =  &s3c_nand_regs->nfdata;
+    bsp_nand->IO_ADDR_W =  &s3c_nand_regs->nfdata;
+    bsp_nand->dev_ready = s3c_dev_ready;
+    bsp_nand->ecc.mode = NAND_ECC_SOFT;
     /*3.硬件相关的操作*/
+    clk_get(NULL, "nand");
+    clk_enable(clk);
+    s3c_nand_regs->nfcont = (TACLS<<12)|(TWRPH0<<8)|(TWRPH1<<4);
+    s3c_nand_regs->nfcont = (1<<1)|(1<<0);
+     
+    
     /*4.使用： nand_scan*/
-    mtd = kzalloc(sizeof(struct mtd), GFP_KERNEL);
-    mtd->priv = nand;
-    mtd->owner = THIS_MODULE;
+    bsp_mtd = kzalloc(sizeof(struct mtd_info), GFP_KERNEL);
+    
+    bsp_mtd->owner = THIS_MODULE;
+    bsp_mtd->priv = bsp_nand;
 
     
-    nand_scan(mtd,1);
+    nand_scan(bsp_mtd,1);
     /*5.add_mtd_partitions*/
+    //add_mtd_partitions(mtd,s3c_nand_parts,4);
+    //add_mtd_device
     
 }
 
 static void nand_exit()
 {
-
+    //del_mtd_partitions(mtd);
+    kfree(bsp_mtd);
+    iounmap(s3c_nand_regs);
+    kfree(bsp_nand);
 }
 
 
