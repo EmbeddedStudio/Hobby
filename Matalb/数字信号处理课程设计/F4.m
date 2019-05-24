67b4a2aad9a9bca21280ef19c5c24b65115c6569
life <2421174533@qq.com>
2019-05-24 10:19:35 +0800

    转移仓库舍弃win7

--- Matalb/数字信号处理课程设计/F4.m
+++ Matalb/数字信号处理课程设计/F4.m
@@ -8,6 +8,7 @@ nz=ny-nx;
 x=x(1:nx);
 y=y(1:ny);
 x1=[x,zeros(1,nz)]; 
-z=x1*1.5+y;
-sound(z,fs);  %²¥·Å     flipud(y)µ¹·Å
+z=x1+y;
+
+sound(z*1.5,fs*2);  %²¥flipud(y);%µ¹·Å  sound(y,fs);
 % audiowrite('Ò£Ô¶Ñ©É½.wav',z,fs) ;
